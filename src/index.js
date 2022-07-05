01223b7ac775510d7c3b4c611b8bda66dac19770
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-05 14:49:25 -0400

    Extra changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SecondsCounter from './SecondsCounter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let i = 0;
+
+window.onload = () => {
+  setInterval(() => {
+    const six = Math.floor((i / 100000) % 10);
+    const five = Math.floor((i / 10000) % 10);
+    const four = Math.floor((i / 1000) % 10);
+    const three = Math.floor((i / 100) % 10);
+    const two = Math.floor((i / 10) % 10);
+    const one = Math.floor((i / 1) % 10);
+    i++;
+    root.render(
+      <SecondsCounter
+        numSix={six}
+        numFive={five}
+        numFour={four}
+        numThree={three}
+        numTwo={two}
+        numOne={one}
+      />
+    );
+  }, 1000);
+}
